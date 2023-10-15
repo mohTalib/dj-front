306be0c7c0f1820f9414de1de3d9352e5c301586
mohTalib <107358717+mohTalib@users.noreply.github.com>
2023-10-15 12:56:49 +0300

    Update settings.py

--- themainweb/settings.py
+++ themainweb/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'club',
     'rest_framework',
-    "corsheaders",
+    
     
  
 ]
@@ -60,7 +60,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'themainweb.urls'
