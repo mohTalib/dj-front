ad2bfac7f51d1274ea19c1a5f76d145c84749656
mohTalib <107358717+mohTalib@users.noreply.github.com>
2023-10-15 13:22:09 +0300

    Update settings.py

--- themainweb/settings.py
+++ themainweb/settings.py
@@ -31,7 +31,9 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     'https://home-opp-club.com',
-    'http://127.0.0.1:3000',
+    'http://127.0.0.1',
+    'http://localhost',
+    
 ]
 
 
